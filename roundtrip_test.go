376c156aa39d86b1d2b153b34209b2d1b8632386
nscuro <nscuro@protonmail.com>
2021-06-22 20:00:28 +0200

    test: use official test resources for roundtrip tests
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- roundtrip_test.go
+++ roundtrip_test.go
@@ -24,6 +24,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -36,61 +37,69 @@ var roundTripSnapshotter = cupaloy.NewDefaultConfig().
 	WithOptions(cupaloy.SnapshotSubdirectory("./testdata/snapshots"))
 
 func TestRoundTripJSON(t *testing.T) {
-	// Read original BOM JSON
-	bomFile, err := os.Open("./testdata/bom-1.2.json")
+	bomFilePaths, err := filepath.Glob("./testdata/*.json")
 	require.NoError(t, err)
-	defer bomFile.Close()
 
-	// Decode BOM
-	bom := new(BOM)
-	require.NoError(t, NewBOMDecoder(bomFile, BOMFileFormatJSON).Decode(bom))
+	for _, bomFilePath := range bomFilePaths {
+		// Read original BOM JSON
+		bomFile, err := os.Open(bomFilePath)
+		require.NoError(t, err)
 
-	// Encode BOM again
-	buf := new(bytes.Buffer)
+		// Decode BOM
+		bom := new(BOM)
+		require.NoError(t, NewBOMDecoder(bomFile, BOMFileFormatJSON).Decode(bom))
+		bomFile.Close()
 
-	tempFile, err := ioutil.TempFile("", "bom.*.json")
-	require.NoError(t, err)
-	defer os.Remove(tempFile.Name())
+		// Encode BOM again
+		buf := new(bytes.Buffer)
+		tempFile, err := ioutil.TempFile("", t.Name()+".*."+filepath.Base(bomFilePath))
+		require.NoError(t, err)
 
-	encoder := NewBOMEncoder(io.MultiWriter(buf, tempFile), BOMFileFormatJSON)
-	encoder.SetPretty(true)
-	require.NoError(t, encoder.Encode(bom))
-	tempFile.Close() // Required for CLI to be able to access the file
+		encoder := NewBOMEncoder(io.MultiWriter(buf, tempFile), BOMFileFormatJSON)
+		encoder.SetPretty(true)
+		require.NoError(t, encoder.Encode(bom))
+		tempFile.Close() // Required for CLI to be able to access the file
 
-	// Sanity checks: BOM has to be valid
-	assertValidBOM(t, tempFile.Name())
+		// Sanity checks: BOM has to be valid
+		assertValidBOM(t, tempFile.Name())
+		os.Remove(tempFile.Name())
 
-	// Compare with snapshot
-	roundTripSnapshotter.SnapshotT(t, buf.String())
+		// Compare with snapshot
+		assert.NoError(t, roundTripSnapshotter.SnapshotMulti(filepath.Base(bomFilePath), buf.String()))
+	}
 }
 
 func TestRoundTripXML(t *testing.T) {
-	// Read original BOM XML
-	bomFile, err := os.Open("./testdata/bom-1.2.xml")
+	bomFilePaths, err := filepath.Glob("./testdata/*.xml")
 	require.NoError(t, err)
-	defer bomFile.Close()
 
-	// Decode BOM
-	bom := new(BOM)
-	require.NoError(t, NewBOMDecoder(bomFile, BOMFileFormatXML).Decode(bom))
+	for _, bomFilePath := range bomFilePaths {
+		// Read original BOM XML
+		bomFile, err := os.Open(bomFilePath)
+		require.NoError(t, err)
 
-	// Encode BOM again
-	buf := new(bytes.Buffer)
+		// Decode BOM
+		bom := new(BOM)
+		require.NoError(t, NewBOMDecoder(bomFile, BOMFileFormatXML).Decode(bom))
+		bomFile.Close()
 
-	tempFile, err := ioutil.TempFile("", "bom.*.xml")
-	require.NoError(t, err)
-	defer os.Remove(tempFile.Name())
+		// Encode BOM again
+		buf := new(bytes.Buffer)
+		tempFile, err := ioutil.TempFile("", t.Name()+".*."+filepath.Base(bomFilePath))
+		require.NoError(t, err)
 
-	encoder := NewBOMEncoder(io.MultiWriter(buf, tempFile), BOMFileFormatXML)
-	encoder.SetPretty(true)
-	require.NoError(t, encoder.Encode(bom))
-	tempFile.Close() // Required for CLI to be able to access the file
+		encoder := NewBOMEncoder(io.MultiWriter(buf, tempFile), BOMFileFormatXML)
+		encoder.SetPretty(true)
+		require.NoError(t, encoder.Encode(bom))
+		tempFile.Close() // Required for CLI to be able to access the file
 
-	// Sanity check: BOM has to be valid
-	assertValidBOM(t, tempFile.Name())
+		// Sanity check: BOM has to be valid
+		assertValidBOM(t, tempFile.Name())
+		os.Remove(tempFile.Name())
 
-	// Compare with snapshot
-	roundTripSnapshotter.SnapshotT(t, buf.String())
+		// Compare with snapshot
+		assert.NoError(t, roundTripSnapshotter.SnapshotMulti(filepath.Base(bomFilePath), buf.String()))
+	}
 }
 
 func assertValidBOM(t *testing.T, bomFilePath string) {
