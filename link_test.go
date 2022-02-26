865f0a934b17919bd48ac7dfefba8877fc62e3e6
nscuro <nscuro@protonmail.com>
2022-02-26 22:12:41 +0100

    more link tests
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- link_test.go
+++ link_test.go
@@ -20,7 +20,9 @@ package cyclonedx
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIsBOMLink(t *testing.T) {
@@ -51,9 +53,81 @@ func TestIsBOMLink(t *testing.T) {
 }
 
 func TestNewBOMLink(t *testing.T) {
-	// TODO
+	bom := NewBOM()
+	bom.SerialNumber = uuid.MustParse("50b69bf2-fd4f-400e-9522-43badebb14ca").URN()
+	bom.Version = 6
+
+	t.Run("Component", func(t *testing.T) {
+		c := Component{BOMRef: "ref"}
+
+		link, err := NewBOMLink(bom, &c)
+		require.NoError(t, err)
+		require.Equal(t, "50b69bf2-fd4f-400e-9522-43badebb14ca", link.SerialNumber.String())
+		require.Equal(t, 6, link.Version)
+		require.Equal(t, "ref", link.Reference)
+	})
+
+	t.Run("Service", func(t *testing.T) {
+		s := Service{BOMRef: "ref"}
+
+		link, err := NewBOMLink(bom, &s)
+		require.NoError(t, err)
+		require.Equal(t, "50b69bf2-fd4f-400e-9522-43badebb14ca", link.SerialNumber.String())
+		require.Equal(t, 6, link.Version)
+		require.Equal(t, "ref", link.Reference)
+	})
+
+	t.Run("Vulnerability", func(t *testing.T) {
+		v := Vulnerability{BOMRef: "ref"}
+
+		link, err := NewBOMLink(bom, &v)
+		require.NoError(t, err)
+		require.Equal(t, "50b69bf2-fd4f-400e-9522-43badebb14ca", link.SerialNumber.String())
+		require.Equal(t, 6, link.Version)
+		require.Equal(t, "ref", link.Reference)
+	})
+}
+
+func TestBOMLink_String(t *testing.T) {
+	t.Run("WithReference", func(t *testing.T) {
+		link := BOMLink{
+			SerialNumber: uuid.MustParse("50b69bf2-fd4f-400e-9522-43badebb14ca"),
+			Version:      6,
+			Reference:    "r/e/f@1.2.3",
+		}
+		require.Equal(t, "urn:cdx:50b69bf2-fd4f-400e-9522-43badebb14ca/6#r%2Fe%2Ff%401.2.3", link.String())
+	})
+
+	t.Run("WithoutReference", func(t *testing.T) {
+		link := BOMLink{
+			SerialNumber: uuid.MustParse("50b69bf2-fd4f-400e-9522-43badebb14ca"),
+			Version:      6,
+			Reference:    "",
+		}
+		require.Equal(t, "urn:cdx:50b69bf2-fd4f-400e-9522-43badebb14ca/6", link.String())
+	})
 }
 
 func TestParseBOMLink(t *testing.T) {
-	// TODO
+	t.Run("WithReference", func(t *testing.T) {
+		link, err := ParseBOMLink("urn:cdx:50b69bf2-fd4f-400e-9522-43badebb14ca/6#r%2Fe%2Ff%401.2.3")
+		require.NoError(t, err)
+		require.Equal(t, uuid.MustParse("50b69bf2-fd4f-400e-9522-43badebb14ca"), link.SerialNumber)
+		require.Equal(t, 6, link.Version)
+		require.Equal(t, "r/e/f@1.2.3", link.Reference)
+	})
+
+	t.Run("WithoutReference", func(t *testing.T) {
+		link, err := ParseBOMLink("urn:cdx:50b69bf2-fd4f-400e-9522-43badebb14ca/6")
+		require.NoError(t, err)
+		require.Equal(t, uuid.MustParse("50b69bf2-fd4f-400e-9522-43badebb14ca"), link.SerialNumber)
+		require.Equal(t, 6, link.Version)
+		require.Equal(t, "", link.Reference)
+	})
+
+	t.Run("Invalid", func(t *testing.T) {
+		link, err := ParseBOMLink("urn:uuid:50b69bf2-fd4f-400e-9522-43badebb14ca")
+		require.Error(t, err)
+		require.Nil(t, link)
+	})
 }
